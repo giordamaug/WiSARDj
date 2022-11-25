9f690a1646967dbc5303a64b69c4159ac077861d
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-11-25 14:31:39 +0100

    updated

--- src/RAMj.jl
+++ src/RAMj.jl
@@ -1,13 +1,27 @@
 ########## WiSARD Types ##########
+module RAMj
 
-struct WRam                 
-    wentry::Dict
-    function getEntry(key::Int128)
-        wentry[key]
-    end
-    function updEntry(key::Int128, value::Real)
-        wentry[key] += 1.0
-    end
+mutable struct WRam                 
+    wentry::Dict{Int64,Float64}
+
+    WRam() = new(Dict())
 end
 
-export WRam
+function getEntry(dt::WRam, key::Int64)
+        if key in keys(dt.wentry)
+            return dt.wentry[key]
+        else
+            return 0.0
+        end
+end
+function updEntry(dt::WRam, key::Int64, value::Float64)
+        if key in keys(dt.wentry)
+            dt.wentry[key] += value
+        else
+            dt.wentry[key] = value
+        end
+end
+
+export WRam, getEntry, updEntry
+
+end
