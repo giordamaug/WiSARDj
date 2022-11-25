44bcdaa4cf459c4fda608b55149a1bd0ff426df1
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-11-25 17:54:17 +0100

    update

--- test.jl
+++ test.jl
@@ -2,7 +2,7 @@
 include("src/WiSARDj.jl")
 using .WiSARDj: WiSARDClassifier, fit!, predict
 
-using CSV, DataFrames, MLJ
+using CSV, DataFrames, MLJ, MLBase
 
 df = CSV.read("/Users/maurizio/WiSARDpy/datasets/iris.csv", DataFrames.DataFrame)
 
@@ -12,6 +12,9 @@ y = vec(Matrix(select(df, [:species])))
 train, test = partition(eachindex(y), 0.8, shuffle=true)
 
 model = WiSARDClassifier()
-fit!(model, X[train,:], y[train,:])
-
-show(stdout::IO, model)
+WiSARDj.fit!(model, X[train,:], y[train,:])
+y_pred = WiSARDj.predict(model, X[test,:])
+accuracy = sum(y_pred .== y[test,:]) / length(y[test,:])
+println("accuracy: $accuracy")
+print(y_pred)
+#show(stdout::IO, model)
