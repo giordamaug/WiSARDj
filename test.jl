13bb4daa063326e91c3369741b0d54d9033790e8
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-11-25 23:42:17 +0100

    update

--- test.jl
+++ test.jl
@@ -2,19 +2,18 @@
 include("src/WiSARDj.jl")
 using .WiSARDj: WiSARDClassifier, fit!, predict
 
-using CSV, DataFrames, MLJ, MLBase
+using CSV, DataFrames, MLJ, MLBase, MLJBase
 
 df = CSV.read("/Users/maurizio/WiSARDpy/datasets/iris.csv", DataFrames.DataFrame)
 
 
-X = Matrix(select(df, Not([:species])))
-y = vec(Matrix(select(df, [:species])))
-train, test = partition(eachindex(y), 0.8, shuffle=true)
+X = Matrix(DataFrames.select(df, Not([:species])))
+y = vec(Matrix(DataFrames.select(df, [:species])))
+train, test = MLJ.partition(eachindex(y), 0.8, shuffle=true, rng=1)
 
 model = WiSARDClassifier()
 WiSARDj.fit!(model, X[train,:], y[train,:])
-y_pred = WiSARDj.predict(model, X[test,:])
-accuracy = sum(y_pred .== y[test,:]) / length(y[test,:])
+ŷ = WiSARDj.predict(model, X[test,:])
+accuracy = sum(ŷ .== y[test,:]) / length(y[test,:])
 println("accuracy: $accuracy")
-print(y_pred)
-#show(stdout::IO, model)
+print(MLJBase.ConfusionMatrix()(ŷ, vec(y[test,:])))
