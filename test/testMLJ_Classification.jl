6d4356b6f8eefee9c98035eea8cf4d13adce1aa9
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-12-01 13:13:39 +0100

    rename

---
+++ test/testMLJ_Classification.jl
@@ -0,0 +1,20 @@
+
+include("../src/WiSARDj.jl")
+using .WiSARDj.MLJInterface: WiSARDClassifier
+
+using CSV, DataFrames, MLJ, MLBase, MLJBase
+
+df = CSV.read("./datasets/iris.csv", DataFrames.DataFrame)
+
+with_cv = true
+model = WiSARDClassifier(n_bits=4, n_tics=16, mapping="linear", bleaching=true)
+
+X = Matrix(DataFrames.select(df, Not([:species])))
+y = vec(Matrix(DataFrames.select(df, [:species])))
+train, test = MLJ.partition(eachindex(y), 0.8, shuffle=true, rng=1)
+MLJ.fit(model, Any, X[train,:], y[train,:])
+ŷ = MLJ.predict(model, Any, X[test,:])
+y_targets = y[test,:]
+accuracy = sum(ŷ .== y_targets) / length(y_targets)
+println("accuracy: $accuracy")
+MLJBase.ConfusionMatrix()(ŷ, coerce(vec(y_targets), OrderedFactor))  # I don't know why we need to coerce! 
